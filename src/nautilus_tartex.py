7a6d8a423f38d40b98a091148a0f6fe460c6069a
Atri Bhattacharya <A.Bhattacharya@uliege.be>
2025-09-27 00:08:02 +0530

    feat: better notifications using libnotify.
    Use libnotify's PyGObject bindings for improved notifications over
    `notify-send`. This makes notifications stack by application, clearly
    shows notifications are from the Files app, and allows showing a header
    and expandable body.

--- src/nautilus_tartex.py
+++ src/nautilus_tartex.py
@@ -5,7 +5,6 @@ import os
 import shutil
 from datetime import datetime
 from pathlib import Path
-from typing import Union
 import threading
 
 # Try to import the Nautilus and GObject libraries.
@@ -14,8 +13,10 @@ import threading
 try:
     import gi  # type: ignore[import-untyped]
     gi.require_version("Gtk", "4.0")
+    gi.require_version("Notify", "0.7")
     from gi.repository import (  # type: ignore[import-untyped]
         Nautilus,
+        Notify,
         GLib,
         GObject,
     )
@@ -28,6 +29,8 @@ class TartexNautilusExtension(GObject.GObject, Nautilus.MenuProvider):
     This extension provides a right-click menu item in Nautilus
     for .tex and .fls files to create a tarball using tartex.
     """
+
+    Notify.init("TarTeX")
 
     def __init__(self):
         os.environ["TERM"] = "dumb"  # suppress rich formatting
@@ -65,7 +68,9 @@ class TartexNautilusExtension(GObject.GObject, Nautilus.MenuProvider):
       ) -> list[Nautilus.MenuItem]:
           return []
 
-    def _run_tartex_process(self, file_obj: Nautilus.FileInfo, notif_id: int):
+    def _run_tartex_process(
+        self,file_obj: Nautilus.FileInfo, n: Notify.Notification
+    ):
         """
         Runs the blocking tartex process in a separate thread.
         This function handles the synchronous part and sends the final
@@ -76,8 +81,9 @@ class TartexNautilusExtension(GObject.GObject, Nautilus.MenuProvider):
             if not tartex_path:
                 GLib.idle_add(
                     self._notify_send,
-                    "Error: tartex command not found in PATH.",
-                    notif_id,
+                    "Error",
+                    "tartex command not found in PATH.",
+                    n
                 )
                 return
 
@@ -105,24 +111,23 @@ class TartexNautilusExtension(GObject.GObject, Nautilus.MenuProvider):
             # Use final summary line upon success for notification
             success_msg = tartex_proc.stdout.splitlines()[-1]
             success_msg = success_msg.replace("Summary: ", "", count=1)
-            GLib.idle_add(self._notify_send, success_msg, notif_id)
+            GLib.idle_add(self._notify_send, "Success", success_msg, n)
 
         except subprocess.CalledProcessError:
-            file_obj.get_parent_info().invalidate_extension_info()
             GLib.idle_add(
                 self._notify_send,
-                f"Error: tartex failed to create archive using {file_path}",
-                notif_id
+                "Error",
+                f"tartex failed to create archive using {file_path}",
+                n,
             )
 
         except Exception:
-            file_obj.get_parent_info().invalidate_extension_info()
             GLib.idle_add(
                 self._notify_send,
-                "Error: An unexpected error occurred",
-                notif_id
+                "Error",
+                " An unexpected error occurred",
+                n
             )
-
 
     def on_tartex_activate(self, menu_item, file_obj):
         """
@@ -130,38 +135,24 @@ class TartexNautilusExtension(GObject.GObject, Nautilus.MenuProvider):
         It starts the tartex command in a new thread to keep the UI responsive.
         """
         # 1. Send an immediate notification that the work has started
-        notification = subprocess.run(
-            [
-                "notify-send",
-                "--app-name",
-                "TarTeX",
-                "Archive creation started (running in background).",
-                "--print-id",
-            ],
-            capture_output=True,
-            text=True,
+        notif = Notify.Notification.new(
+            "TarTeX",
+            "Archive creation started (running in background).",
         )
-        notif_id = int(notification.stdout)
+        notif.show()
 
         # 2. Start the blocking process in a new thread
         thread = threading.Thread(
             target=self._run_tartex_process,
-            args=(file_obj, notif_id),
+            args=(file_obj, notif),
             daemon=True,  # Ensures the thread exits if the main app is closed
         )
         thread.start()
         file_obj.invalidate_extension_info()
 
-    def _notify_send(self, msg: str, notif_id: Union[int, None] = None):
+    def _notify_send(self, head: str, msg: str, n: Notify.Notification):
         """Send notification at end of process one way or another
 
-        :msg: notification message
-        :notify_id: notification id to replace (optional)
-        :returns: TODO
-
         """
-        cmdline = ["notify-send", msg, "--app-name", "TarTeX"]
-        if notif_id:
-            cmdline += ["--replace-id", f"{notif_id}"]
-        subprocess.run(cmdline)
-
+        n.update(head, msg)
+        n.show()
